763b2c95d4c8488af039b2e336361b46bf2047c6
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2024-04-09 18:07:53 -0300

    add plot for example milp

--- plots.py
+++ plots.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 import scienceplots
 
+from matplotlib.patches import Polygon
 
-plt.style.use(['science','ieee','vibrant'])
+plt.style.use(['science','vibrant'])
+plt.rcParams.update({
+    'font.size': 12,
+    'font.family': 'sans-serif',
+    # 'figure.figsize' : (8, 6),
+    'figure.dpi' : 300,
+})
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
 
 if __name__ == '__main__':
@@ -16,4 +25,55 @@ if __name__ == '__main__':
     plt.ylabel('Count')
 
     # plt.show()
-    plt.savefig('pictures/scopus.pdf', dpi=300)
+    plt.savefig('pictures/scopus.pdf')
+    plt.figure()
+
+    # Problem definition
+    # min -y
+    # s.t. z <= y
+    #      z + y <= 5
+    #      y >= 0
+    #      z \in \Z
+
+    # ax = plt.axes()
+    # ax.add_patch(Polygon([[0., 0.], [2.5, 2.5], [5., 0.], [0., 0.]], facecolor='b', edgecolor='k'))
+    # plt.fill([0., 2.5, 5., 0.], [0., 2.5, 0., 0.], colors[2], edgecolor=colors[1], label=r"$A\boldsymbol{x} \le \boldsymbol{b}$")
+    # plt.fill([0., 2.5, 5., 0.], [0., 2.5, 0., 0.], colors[2], alpha=0.5, label=r"$A\boldsymbol{x} \le \boldsymbol{b}$")
+    plt.fill([0., 2.5, 5., 0.], [0., 2.5, 0., 0.], facecolor='w', edgecolor=colors[2], linestyle='--', linewidth=0.5, label=r"$A\boldsymbol{x} \le \boldsymbol{b}$")
+    x_feasible = np.arange(0, 5, 1)
+    y_max = np.min([5 - x_feasible, x_feasible], 0)
+    plt.vlines(x_feasible, np.zeros(len(x_feasible)), y_max, colors[1], 'solid', capstyle='round', label=r"$\mathcal{X}$")
+    plt.ylim(0, 4.5)
+    plt.xticks(np.arange(0, 6, 1))
+
+    plt.gca().spines[["left", "bottom"]].set_position(("data", 0))
+    plt.gca().spines[['right', 'top']].set_visible(False)
+    plt.gca().tick_params(
+        axis='both',
+        direction='inout',
+        which='major',
+        top=False,
+        right=False,
+        bottom=True,
+        left=True,
+    )
+    plt.gca().tick_params(
+        axis='both',
+        which='minor',
+        top=False,
+        right=False,
+        bottom=False,
+        left=False,
+    )
+    xlim = plt.xlim()
+    plt.gca().spines["bottom"].set_bounds(low=0, high=xlim[1])
+    ylim = plt.ylim()
+    plt.gca().spines["left"].set_bounds(low=0, high=ylim[1])
+    plt.plot(1, 0, ">k", markersize=2, transform=plt.gca().get_yaxis_transform(), clip_on=False)
+    plt.plot(0, 1, "^k", markersize=2, transform=plt.gca().get_xaxis_transform(), clip_on=False)
+    plt.xlabel('$z$')
+    plt.ylabel('$y$')
+    plt.legend()
+
+    # plt.show()
+    plt.savefig('pictures/milp_example_feasible_region.pdf')
